33c2909f5bccb4fafe6da0ab4f28f786b9dde96e
reality77 <davmail08-git@yahoo.fr>
2018-11-02 16:51:25 +0100

    [INFLUXDB] Mise en place influx db metrics

--- web/Startup.cs
+++ web/Startup.cs
@@ -11,7 +11,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Globalization;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
 using web.Filters;
+using App.Metrics;
+using App.Metrics.Extensions.Reporting.InfluxDB;
+using App.Metrics.Extensions.Reporting.InfluxDB.Client;
+using App.Metrics.Reporting.Abstractions;
+using App.Metrics.Reporting.Interfaces;
+using App.Metrics.Configuration;
 
 namespace web
 {
@@ -32,10 +39,35 @@ namespace web
             services.AddDbContext<dal.RCBenevoleContext>(options =>
                 options.UseNpgsql(connectionString));
 
+
+            var influxdb_database = "appmetricsdemo" ;
+            var influxdb_uri = new Uri("http://influxdb:8086");
+
+            services.AddMetrics(options =>
+                {
+                    options.WithGlobalTags((globalTags,info) =>
+                    {
+                        globalTags.Add("app", info.EntryAssemblyName);
+                    });
+                })
+            .AddHealthChecks()
+            .AddReporting(
+                factory =>
+                {
+                    factory.AddInfluxDb(
+                        new  InfluxDBReporterSettings
+                        {
+                            InfluxDbSettings = new InfluxDBSettings(influxdb_database, influxdb_uri),
+                            ReportInterval = TimeSpan.FromSeconds(5)
+                        });
+                })
+            .AddMetricsMiddleware(/*options => options.IgnoredHttpStatusCodes = new[] { 404 }*/);
+
             services.AddMvc(options =>
             {
                 options.Filters.Add(new RequestLogFilter());
                 options.Filters.Add(new MaintenanceModeFilter());
+                options.AddMetricsResourceFilter();
             });
 
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
@@ -49,7 +81,7 @@ namespace web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime)
         {
 			// Pour utilisation d'un sous-répertoire via nginx (rendre configurable pour docker par variable d'environnement)
 			var pathBase = Environment.GetEnvironmentVariable("APP_PATH_BASE");
@@ -76,6 +108,9 @@ namespace web
             app.UseRequestLocalization(BuildLocalizationOptions());
             
             app.UseAuthentication();
+
+            app.UseMetrics();
+            app.UseMetricsReporting(lifetime);
 
             app.UseMvc(routes =>
             {
